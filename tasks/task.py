96232b851ddaada96124c4c27983f80bf242574e
Gruzin <gruzd.roman@inbox.ru>
2023-03-10 17:29:02 +0300

    Commit with a solution

--- tasks/task.py
+++ tasks/task.py
@@ -2,4 +2,13 @@ from typing import List, Tuple
 
 def sort_unique_elements(str_list: Tuple[str]) -> List[str]:
     # TODO: Add your code here
-    return []
+    new_list = []
+    for i in str_list:
+        new_list.append(i)
+        if new_list.count(i) > 1:
+            new_list.pop()
+        new_list.sort()
+    return new_list
+my_list = ('red', 'white', 'black', 'red', 'green', 'black')
+
+sort_unique_elements(my_list)
